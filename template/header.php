e882d2ad11244e6d7707f82b2aee6ebd90285895
JordyV23 <jordyvargas0123@gmail.com>
2023-05-19 15:22:15 -0600

    Commit inicial

---
+++ template/header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>The Librarian</title>
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="icon" href="../public/icon.svg" type="image/svg+xml">
+    <script src="https://kit.fontawesome.com/8633cf8400.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#" style="font-size: 30px;"> <i class="fa-solid fa-book-atlas"
+                    style="color: #f39c12;"></i> The Librarian</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01"
+                aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarColor01">
+                <ul class="navbar-nav me-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/index.php"><i class="fa-solid fa-house"></i> Inicio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"><i class="fa-solid fa-book-bookmark"></i> Libros</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/about.php"><i class="fa-brands fa-redhat"></i> Nosotros</a>
+                    </li>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container">
+        <div class="row">
