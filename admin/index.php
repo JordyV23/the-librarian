77922174a497245773ba03c343d3acf5f5c34add
JordyV23 <jordyvargas0123@gmail.com>
2023-05-20 18:59:49 -0600

    Tareas realizadas -Diseño basico del login -Diseño basico del panel de administrador -Separado de header y footer del panel de administrador

--- admin/index.php
+++ admin/index.php
@@ -1,17 +1,76 @@
-<!DOCTYPE html>
+<?php
+if ($_POST) {
+    header('Location:inicio.php');
+}
+
+?>
+
+<!doctype html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin</title>
+    <title>WebAdmin</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 
 <body>
 
-    Login
+    <div class="container">
+        <div class="row">
 
+            <div class="col-md-4">
+
+            </div>
+
+            <div class="col-md-4">
+                <br />
+                <br />
+                <br />
+                <div class="card">
+                    <div class="card-header">
+                        Inicio de Sesion
+                    </div>
+                    <div class="card-body">
+                        <form method="POST">
+                            <div class="form-group">
+                                <label><i class="fa-solid fa-user"></i> Usuario</label=>
+                                    <input type="text" class="form-control" -describedby="emailHelp"
+                                        placeholder="Ingrese su nombre de usuario" name="usuario">
+                            </div>
+
+                            <div class="form-group">
+                                <label><i class="fa-solid fa-key"></i> Contraseña</label>
+                                <input type="password" class="form-control" placeholder="Contraseña" name="contrasenia">
+                            </div>
+                            <button type="submit" class="btn btn-primary"><i class="fa-solid fa-user-check"></i> Iniciar Sesion</button>
+                        </form>
+
+
+                    </div>
+                    <div class="card-footer text-muted">
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <script src="https://kit.fontawesome.com/8633cf8400.js" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
 </body>
 
 </html>
